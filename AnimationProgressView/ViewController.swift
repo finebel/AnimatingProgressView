81d8364c16b0de2dad9fdf2004ed0549bff63975
finebel <finebel.fl@gmail.com>
2020-08-19 16:54:08 +0200

    Initial Commit

---
+++ AnimationProgressView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimationProgressView
+//
+//  Created by finebel on 19.08.20.
+//  Copyright © 2020 finebel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
